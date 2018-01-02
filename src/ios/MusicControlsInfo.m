7cb1c56ab94af883133f988b1f708854fc8aeeb1
rajan.shah <rajan.shah@solutionanalysts.com>
2018-01-02 11:12:42 +0530

    Solved crash issue in initWithDictionary

--- src/ios/MusicControlsInfo.m
+++ src/ios/MusicControlsInfo.m
@@ -12,19 +12,25 @@
 
 - (id) initWithDictionary: (NSDictionary *) dictionary {
     
-    if (self = [super init]) {
+   if (self = [super init]) {
         [self setArtist: [dictionary objectForKey:@"artist"]];
         [self setTrack: [dictionary objectForKey:@"track"]];
         [self setAlbum: [dictionary objectForKey:@"album"]];
         [self setTicker: [dictionary objectForKey:@"ticker"]];
         [self setCover: [dictionary objectForKey:@"cover"]];
-        [self setDuration: [[dictionary objectForKey:@"duration"] integerValue]];
-        [self setElapsed: [[dictionary objectForKey:@"elapsed"] integerValue]];
+        if(![[dictionary valueForKey:@"duration"] isEqual:[NSNull null]] && [dictionary valueForKey:@"duration"] != nil){
+            NSNumber *duration = [NSNumber numberWithInt:[[dictionary valueForKey:@"duration"]intValue]];
+            [self setDuration: [duration intValue]];
+        }
+        if(![[dictionary valueForKey:@"elapsed"] isEqual:[NSNull null]] && [dictionary valueForKey:@"elapsed"] != nil){
+            [self setElapsed: [[dictionary objectForKey:@"elapsed"] intValue]];
+        }
         [self setIsPlaying: [[dictionary objectForKey:@"isPlaying"] boolValue]];
         [self setHasPrev: [[dictionary objectForKey:@"hasPrev"] boolValue]];
         [self setHasNext: [[dictionary objectForKey:@"hasNext"] boolValue]];
         [self setDismissable: [[dictionary objectForKey:@"dismissable"] boolValue]];
     }
+    
     
     return self;
 }
